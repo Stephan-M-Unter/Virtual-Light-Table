f92bb5ac9432f6edecaf724c86fde7f4daac7ab3
Stephan M. Unter <social@stephan-unter.de>
2022-01-18 12:49:57 +0100

    re-implementation of polygon mode

--- renderer/upload.js
+++ renderer/upload.js
@@ -10,38 +10,19 @@ let currentUpload = null;
 let mode = $('.active_mode').attr('mode');
 let maskMode = 'no_mask';
 let scaleMode = null;
+let actionMode = null;
 let scalePoint = null;
 let scale = 1;
 const mousestart = {};
 
-let recto = {
-  'stage': new createjs.Stage('recto_canvas'),
-  'canvas': $('#recto_canvas'),
-  'sidename': 'recto',
-  'content': {
-    'filepath': null,
-    'img': null,
-    'img_bg': null,
-    'rotation': 0,
-  },
-};
-let verso = {
-  'stage': new createjs.Stage('verso_canvas'),
-  'canvas': $('#verso_canvas'),
-  'sidename': 'verso',
-  'content': {
-    'filepath': null,
-    'img': null,
-    'img_bg': null,
-    'rotation': 0,
-  },
-};
+let recto;
+let verso;
+createEmptySide('recto');
+createEmptySide('verso');
 
 /* DOCUMENT READY */
 
 $(document).ready(function() {
-  recto.stage.sidename = 'recto';
-  verso.stage.sidename = 'verso';
   updateCanvasSize();
 });
 
@@ -88,7 +69,21 @@ function createEmptySide(sidename) {
       'img_bg': null,
       'rotation': 0,
     },
+    'mask': {
+      'group': null,
+      'box': [],
+      'polygon': [],
+      'auto': {},
+    },
   };
+  newSide.stage.sidename = sidename;
+  newSide.stage.enableMouseOver();
+
+  const maskGroup = new createjs.Container();
+  maskGroup.name = 'mask container';
+  newSide.mask.group = maskGroup;
+  newSide.stage.addChild(maskGroup);
+
   if (sidename == 'recto') {
     recto = newSide;
   } else if (sidename == 'verso') {
@@ -131,10 +126,11 @@ function draw(sidename) {
 
       // creating white "shadow" layer to visually indicate mask
       const shadow = new createjs.Shape();
+      shadow.name = 'Shadow';
       shadow.graphics.beginFill('white')
           .drawRect(0, 0, cWidth, cHeight)
           .endFill();
-      shadow.alpha = 0.7;
+      shadow.alpha = 0.8;
       shadow.side = side;
 
       // shadow event listeners
@@ -145,11 +141,13 @@ function draw(sidename) {
         handlePressMove(event, event.target.side.sidename);
       });
 
-      side.stage.addChildAt(side.content.img_bg, shadow, side.content.img, 0);
+      side.stage.addChildAt(side.content.img_bg, shadow, side.content.img, side.mask.group, 0);
+      // side.canvas.attr('title', side.content.filepath);
       //   drawMasks();
     }
   }
   side.stage.update();
+  drawMasks();
   checkGUI();
 }
 
@@ -167,7 +165,9 @@ function createImage(sidename) {
 
     // create new Bitmap objects
     const image = new createjs.Bitmap(newImage);
+    image.name = 'image';
     const imageBackground = new createjs.Bitmap(newImage);
+    imageBackground.name = 'image_background';
     side.content.img = image;
     side.content.img_bg = imageBackground;
 
@@ -223,8 +223,10 @@ function readExifPPI(image, sidename) {
  */
 function handleMouseDown(event, sidename) {
   const side = getSide(sidename);
-  if (sidename == scaleMode) {
+  if (actionMode == 'scale' && sidename == scaleMode) {
     doScaling(event.stageX, event.stageY);
+  } else if (actionMode == 'polygon') {
+    addPolygonNode([event.stageX, event.stageY], sidename);
   }
   mousestart.x = event.stageX;
   mousestart.y = event.stageY;
@@ -242,21 +244,23 @@ function handlePressMove(event, sidename) {
   const mouse = {x: event.stageX, y: event.stageY};
   const mouseDistance = {x: mouse.x - mousestart.offsetX, y: mouse.y - mousestart.offsetY};
 
-  if (mode == 'move') {
-    side.content.x = mouseDistance.x;
-    side.content.y = mouseDistance.y;
-  } else if (mode == 'rotate') {
-    const radsOld = Math.atan2(mousestart.y - side.content.img.y,
-        mousestart.x - side.content.img.x);
-    const radsNew = Math.atan2(mouse.y - side.content.img.y,
-        mouse.x - side.content.img.x);
-    const rads = radsNew - radsOld;
-    const deltaAngle = rads * (180 / Math.PI);
-    rotateByAngle(deltaAngle, sidename);
-    mousestart.x = mouse.x;
-    mousestart.y = mouse.y;
+  if (actionMode == null) {
+    if (mode == 'move') {
+      side.content.x = mouseDistance.x;
+      side.content.y = mouseDistance.y;
+    } else if (mode == 'rotate') {
+      const radsOld = Math.atan2(mousestart.y - side.content.img.y,
+          mousestart.x - side.content.img.x);
+      const radsNew = Math.atan2(mouse.y - side.content.img.y,
+          mouse.x - side.content.img.x);
+      const rads = radsNew - radsOld;
+      const deltaAngle = rads * (180 / Math.PI);
+      rotateByAngle(deltaAngle, sidename);
+      mousestart.x = mouse.x;
+      mousestart.y = mouse.y;
+    }
+    draw(sidename);
   }
-  draw(sidename);
 }
 
 /**
@@ -281,7 +285,6 @@ function handleMousewheel(event) {
         rectoScale = Math.round(rectoScale*100) / 100;
         recto.content.img.scale = rectoScale;
         recto.content.img_bg.scale = rectoScale;
-        console.log("Recto:", recto.content.img.scale);
       }
       if (verso.content.img) {
         let versoPPI = $('#verso_ppi').val();
@@ -292,11 +295,9 @@ function handleMousewheel(event) {
         versoScale = Math.round(versoScale*100) / 100;
         verso.content.img.scale = versoScale;
         verso.content.img_bg.scale = versoScale;
-        console.log("Verso:", verso.content.img.scale);
       }
       recto.stage.update();
       verso.stage.update();
-      console.log("Scale:", scale);
     }
   }
 }
@@ -326,6 +327,12 @@ function swap() {
   let temp = recto.content;
   recto.content = verso.content;
   verso.content = temp;
+
+  // swap masks
+  temp = recto.mask;
+  recto.mask = verso.mask;
+  verso.mask = temp;
+  drawMasks();
 
   // swap ppi
   temp = $('#recto_ppi').val();
@@ -453,28 +460,33 @@ function rotateToAngle(targetAngle, sidename) {
 }
 
 /**
- * 
- * @param {'recto'|'verso'} target 
+ *
+ * @param {'recto'|'verso'} target
  */
 function startScaling(target) {
+  actionMode = 'scale';
   scaleMode = target;
-  console.log("Starting Scale Mode for:", scaleMode);
-
 }
 
+/**
+ *
+ */
 function endScaling() {
-  console.log("Ending Scale Mode for:", scaleMode);
+  actionMode = null;
   scaleMode = null;
   scalePoint = null;
   $('.measure').removeClass('active');
   $('canvas').removeClass('scale');
-
 }
 
+/**
+ *
+ * @param {*} x
+ * @param {*} y
+ */
 function doScaling(x, y) {
   if (scalePoint == null) {
     // this click determines the first point
-    console.log("First point:", x, y);
     scalePoint = [x, y];
   } else {
     // this is the second point, enough to determine the distance
@@ -491,6 +503,9 @@ function doScaling(x, y) {
   }
 }
 
+/**
+ *
+ */
 function scaleImages() {
   if (recto.content.filepath) {
     const rectoPPI = $('#recto_ppi').val();
@@ -512,6 +527,429 @@ function scaleImages() {
   }
   recto.stage.update();
   verso.stage.update();
+}
+
+/**
+ *
+ */
+function drawMasks() {
+  if (maskMode == 'boundingbox') {
+    // display boundingbox
+    drawBoxMask();
+  } else if (maskMode == 'polygon') {
+    // display polygonal mask
+    drawPolygonMask();
+  } else if (maskMode == 'automatic') {
+    // use ML result
+    drawAutoMask();
+  } else {
+    // no mask -> un-display all masks
+    clearMask();
+  }
+}
+
+/**
+ *
+ */
+function setDefaultBox() {
+  // no mask created so far, set default values
+  const canvasWidth = recto.stage.canvas.width;
+  const canvasHeight = recto.stage.canvas.height;
+  const x = canvasWidth * 0.25;
+  const y = canvasHeight * 0.25;
+  const w = canvasWidth * 0.5;
+  const h = canvasHeight * 0.5;
+
+  // nw, sw, se, ne
+  recto.mask.box = [[x, y], [x, y+h], [x+w, y+h], [x+w, y]];
+  updateVersoMask();
+}
+
+/**
+ *
+ */
+function drawBoxMask() {
+  if (recto.mask.box.length == 0) {
+    // no mask created so far, set default values
+    setDefaultBox();
+  }
+
+  // RECTO
+  if (recto.content.img) {
+    const polygonRecto = createPolygon(recto.mask.box);
+    recto.content.img.mask = polygonRecto;
+    const b1rt = createVertex(recto.mask.box[0]);
+    const b2rt = createVertex(recto.mask.box[1]);
+    const b3rt = createVertex(recto.mask.box[2]);
+    const b4rt = createVertex(recto.mask.box[3]);
+    b1rt.on('pressmove', (event) => {
+      resizeBox(event, 'recto', 'nw');
+    });
+    b2rt.on('pressmove', (event) => {
+      resizeBox(event, 'recto', 'sw');
+    });
+    b3rt.on('pressmove', (event) => {
+      resizeBox(event, 'recto', 'se');
+    });
+    b4rt.on('pressmove', (event) => {
+      resizeBox(event, 'recto', 'ne');
+    });
+    recto.mask.group.removeAllChildren();
+    recto.mask.group.addChild(b1rt, b2rt, b3rt, b4rt);
+    recto.stage.update();
+  }
+
+  // VERSO
+  if (verso.content.img) {
+    const polygonVerso = createPolygon(verso.mask.box);
+    verso.content.img.mask = polygonVerso;
+    const b1vs = createVertex(verso.mask.box[0]);
+    const b2vs = createVertex(verso.mask.box[1]);
+    const b3vs = createVertex(verso.mask.box[2]);
+    const b4vs = createVertex(verso.mask.box[3]);
+    b1vs.on('pressmove', (event) => {
+      resizeBox(event, 'verso', 'nw');
+    });
+    b2vs.on('pressmove', (event) => {
+      resizeBox(event, 'verso', 'sw');
+    });
+    b3vs.on('pressmove', (event) => {
+      resizeBox(event, 'verso', 'se');
+    });
+    b4vs.on('pressmove', (event) => {
+      resizeBox(event, 'verso', 'ne');
+    });
+    verso.mask.group.removeAllChildren();
+    verso.mask.group.addChild(b1vs, b2vs, b3vs, b4vs);
+    verso.stage.update();
+  }
+}
+
+/**
+ *
+ * @param {*} event
+ * @param {*} sidename
+ * @param {*} compass
+ */
+function resizeBox(event, sidename, compass) {
+  let mouseX = event.stageX;
+  if (sidename == 'verso') {
+    mouseX = recto.stage.canvas.width - mouseX;
+  }
+  const mouseY = event.stageY;
+
+  let x = recto.mask.box[0][0];
+  let y = recto.mask.box[0][1];
+  let w = recto.mask.box[2][0];
+  let h = recto.mask.box[2][1];
+
+  if (compass == 'nw') {
+    x = Math.min(mouseX, w);
+    y = Math.min(mouseY, h);
+  } else if (compass == 'sw') {
+    x = Math.min(mouseX, w);
+    h = Math.max(y, mouseY);
+  } else if (compass == 'se') {
+    w = Math.max(mouseX, x);
+    h = Math.max(mouseY, y);
+  } else if (compass == 'ne') {
+    w = Math.max(mouseX, x);
+    y = Math.min(mouseY, h);
+  }
+
+  recto.mask.box = [[x, y], [x, h], [w, h], [w, y]];
+  updateVersoMask();
+  drawBoxMask();
+}
+
+/**
+ *
+ * @param {*} pointsArray
+ * @returns
+ */
+function createPolygon(pointsArray) {
+  if (pointsArray.length > 0) {
+    const polygon = new createjs.Shape();
+    const p0 = pointsArray[0];
+    polygon.graphics.moveTo(p0[0], p0[1]);
+    for (let i = 1; i < pointsArray.length; i++) {
+      const p = pointsArray[i];
+      polygon.graphics.lineTo(p[0], p[1]);
+    }
+    polygon.graphics.lineTo(p0[0], p0[1]);
+    return polygon;
+  } else {
+    return null;
+  }
+}
+
+/**
+ *
+ * @param {*} point
+ * @param {*} circle
+ * @returns
+ */
+function createVertex(point, circle=false) {
+  const size = 10;
+  const x = point[0] - size/2;
+  const y = point[1] - size/2;
+  const vertex = new createjs.Shape();
+  vertex.graphics.setStrokeStyle(1).beginStroke('green');
+  vertex.graphics.beginFill('lightgreen');
+  if (circle) {
+    vertex.graphics.drawCircle(point[0], point[1], size*0.66);
+  } else {
+    vertex.graphics.drawRect(x, y, size, size);
+  }
+
+  vertex.on('mouseover', vertexMouseIn);
+  vertex.on('mouseout', vertexMouseOut);
+
+  return vertex;
+}
+
+/**
+ *
+ * @param {*} event
+ */
+function vertexMouseIn(event) {
+  const c = event.target.graphics.command;
+  event.target.graphics.clear();
+  event.target.graphics.beginFill('green');
+  if ('radius' in c) {
+    event.target.graphics.drawCircle(c.x, c.y, c.radius);
+  } else {
+    event.target.graphics.drawRect(c.x, c.y, c.w, c.h);
+  }
+  recto.stage.update();
+  verso.stage.update();
+}
+
+/**
+ *
+ * @param {*} event
+ */
+function vertexMouseOut(event) {
+  const c = event.target.graphics.command;
+  event.target.graphics.clear();
+  event.target.graphics.beginFill('lightgreen');
+  event.target.graphics.setStrokeStyle(1).beginStroke('green');
+  if ('radius' in c) {
+    event.target.graphics.drawCircle(c.x, c.y, c.radius);
+  } else {
+    event.target.graphics.drawRect(c.x, c.y, c.w, c.h);
+  }
+  recto.stage.update();
+  verso.stage.update();
+}
+
+/**
+ *
+ */
+function mirrorPoints(pointArray) {
+  const canvasWidth = recto.stage.canvas.width;
+  const result = [];
+  pointArray.forEach((point) => {
+    result.push([canvasWidth-point[0], point[1]]);
+  });
+  result.reverse();
+  return result;
+}
+
+/**
+ *
+ */
+function drawPolygonMask() {
+  // RECTO
+  if (recto.content.img) {
+    const polygonRecto = createPolygon(recto.mask.polygon);
+    recto.content.img.mask = polygonRecto;
+    recto.mask.group.removeAllChildren();
+    recto.mask.polygon.forEach((point, index) => {
+      let vertex;
+      if (index == recto.mask.polygon.length - 1) {
+        vertex = createVertex(point, true);
+      } else {
+        vertex = createVertex(point);
+      }
+      vertex.on('pressmove', (event) => {
+        moveVertex(event, 'recto', index);
+      });
+      vertex.on('click', () => {
+        if (actionMode == 'polygon_remove') {
+          removePolygonNode(index);
+        }
+      });
+      recto.mask.group.addChild(vertex);
+    });
+    recto.stage.update();
+  }
+
+  // VERSO
+  if (verso.content.img) {
+    const polygonVerso = createPolygon(verso.mask.polygon);
+    verso.content.img.mask = polygonVerso;
+    verso.mask.group.removeAllChildren();
+    verso.mask.polygon.forEach((point, index) => {
+      let vertex;
+      if (index == 0) {
+        vertex = createVertex(point, true);
+      } else {
+        vertex = createVertex(point);
+      }
+      vertex.on('pressmove', (event) => {
+        moveVertex(event, 'verso', verso.mask.polygon.length-index-1);
+      });
+      vertex.on('click', () => {
+        if (actionMode == 'polygon_remove') {
+          removePolygonNode(verso.mask.polygon.length-index-1);
+        }
+      });
+      verso.mask.group.addChild(vertex);
+    });
+    verso.stage.update();
+  }
+}
+
+/**
+ *
+ * @param {*} event
+ * @param {*} sidename
+ * @param {*} index
+ */
+function moveVertex(event, sidename, index) {
+  let mouseX = event.stageX;
+  if (sidename == 'verso') {
+    mouseX = recto.stage.canvas.width - mouseX;
+  }
+  const mouseY = event.stageY;
+
+  recto.mask.polygon[index] = [mouseX, mouseY];
+  updateVersoMask();
+  drawMasks();
+}
+
+/**
+ * 
+ */
+function clearPolygon() {
+  recto.mask.polygon = [];
+  verso.mask.polygon = [];
+  drawMasks();
+}
+
+/**
+ * 
+ */
+function undoPolygonNode() {
+  if (recto.mask.polygon.length > 0) {
+    recto.mask.polygon.pop();
+    updateVersoMask();
+  }
+  drawMasks();
+}
+
+/**
+ * 
+ * @param {*} point 
+ * @param {*} sidename 
+ */
+function addPolygonNode(point, sidename) {
+  let x = point[0];
+  if (sidename == 'verso') {
+    x = recto.stage.canvas.width - x;
+  }
+  const y = point[1];
+  recto.mask.polygon.push([x,y]);
+  updateVersoMask();
+  drawMasks();
+}
+
+/**
+ * 
+ * @param {*} index 
+ */
+function removePolygonNode(index) {
+  recto.mask.polygon.splice(index, 1);
+  updateVersoMask();
+  drawMasks();
+}
+
+/**
+ * 
+ */
+function updateVersoMask() {
+  verso.mask.box = mirrorPoints(recto.mask.box);
+  verso.mask.polygon = mirrorPoints(recto.mask.polygon);
+}
+
+/**
+ * 
+ */
+function startAddPolygonNodes() {
+  endActiveModes();
+  actionMode = 'polygon';
+  $('#mask_control_polygon_add').addClass('active');
+  $('canvas').addClass('addPolygonNode');
+}
+
+/**
+ * 
+ */
+function endAddPolygonNodes() {
+  actionMode = null;
+  $('.addPolygonNode').removeClass('addPolygonNode');
+  $('#mask_control_polygon_add').removeClass('active');
+}
+
+/**
+ * 
+ */
+function startRemovePolygonNodes() {
+  endActiveModes();
+  actionMode = 'polygon_remove';
+  $('#mask_control_polygon_remove').addClass('active');
+  $('canvas').addClass('removePolygonNode');
+}
+
+/**
+ * 
+ */
+function endRemovePolygonNodes() {
+  actionMode = null;
+  $('.removePolygonNode').removeClass('removePolygonNode');
+  $('#mask_control_polygon_remove').removeClass('active');
+}
+
+/**
+ * 
+ */
+function endActiveModes() {
+  endAddPolygonNodes();
+  endRemovePolygonNodes();
+}
+
+/**
+ *
+ */
+function drawAutoMask() {
+
+}
+
+/**
+ *
+ */
+function clearMask() {
+  if (recto.content.img) {
+    recto.content.img.mask = null;
+    recto.mask.group.removeAllChildren();
+    recto.stage.update();
+  }
+  if (verso.content.img) {
+    verso.content.img.mask = null;
+    verso.mask.group.removeAllChildren();
+    verso.stage.update();
+  }
 }
 
 
@@ -611,12 +1049,40 @@ $('.list_item').on('click', (event) => {
       listItem = listItem.parent();
     }
     listItem.addClass('selected');
+    if (maskMode == 'polygon' && listItem.attr('mask_mode') != 'polygon') {
+      endAddPolygonNodes();
+    }
     maskMode = listItem.attr('mask_mode');
     $('.mask_controls.'+maskMode).addClass('selected');
     $('.mask_explanation.'+maskMode).addClass('selected');
-    console.log('Mask Mode:', maskMode);
+    drawMasks();
   } else {
     list.addClass('open');
+  }
+});
+
+$('#mask_control_box_reset').click(() => {
+  setDefaultBox();
+  drawMasks();
+});
+$('#mask_control_polygon_undo').click(() => {
+  undoPolygonNode();
+});
+$('#mask_control_polygon_clear').click(() => {
+  clearPolygon();
+});
+$('#mask_control_polygon_add').click(() => {
+  if ($('#mask_control_polygon_add').hasClass('active')) {
+    endAddPolygonNodes();
+  } else {
+    startAddPolygonNodes();
+  }
+});
+$('#mask_control_polygon_remove').click(() => {
+  if ($('#mask_control_polygon_remove').hasClass('active')) {
+    endRemovePolygonNodes();
+  } else {
+    startRemovePolygonNodes();
   }
 });
 
