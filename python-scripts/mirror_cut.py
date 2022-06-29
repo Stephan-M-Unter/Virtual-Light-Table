f5ef3c7c7ba7a2b3786c9c077d853b792f98b9f9
Stephan M. Unter <social@stephan-unter.de>
2022-06-29 14:47:03 +0200

    correction for shadow calculation

--- python-scripts/mirror_cut.py
+++ python-scripts/mirror_cut.py
@@ -20,6 +20,11 @@ points = sys.argv[2]
 if points == 'no_mask':
     mirror = np.zeros((image.size[1], image.size[0], 4))
     mirror[:,:,3] = 255
+    mask_orig = np.array(image)[:,:,3]
+    mask_orig = np.flip(mask_orig, axis=1)
+    mask_orig[mask_orig != 0] = 255
+    mask_orig = 255-mask_orig
+    mirror[:,:,3] -= mask_orig
 else:
     points = json.loads(points)
     points = [(int(p['x']), int(p['y'])) for p in points]
@@ -31,21 +36,22 @@ else:
     lower = max(ys)
     mask = Image.new('L', (image.size[0], image.size[1]), 0)
     ImageDraw.Draw(mask).polygon(points, outline=1, fill=1)
-    image = np.array(image)
-    image[:,:,3] = np.array(mask)*255
+    image_array = np.array(image)
+    image_array[:,:,:3] = 0
+    image_array[:,:,3] = np.array(mask)*255
 
-    image = Image.fromarray(image)
+    mask_orig = np.array(image)[:,:,3]
+    # mask_orig = np.flip(mask_orig, axis=1)
+    mask_orig[mask_orig != 0] = 1
+    image_array[:,:,3] *= mask_orig
+
+    image = Image.fromarray(image_array)
+
     mirror = np.array(image.crop((left, upper, right, lower))).astype('float64')
-    mirror[:,:,:3] /= 180
     mirror = np.flip(mirror, axis=1)
 
-mask_orig = np.array(image)[:,:,3]
-mask_orig = np.flip(mask_orig, axis=1)
-mask_orig[mask_orig != 0] = 255
-mask_orig = 255-mask_orig
-mirror[:,:,3] -= mask_orig
-mirror = mirror.astype("uint8")
 
+mirror = mirror.astype("uint8")
 new_filename = f"{image_name}_mirror.{extension}"
 vlt_folder = os.path.join(os.getenv('APPDATA'), "Virtual Light Table", "temp", "imgs")
 new_path = os.path.join(vlt_folder, new_filename)
