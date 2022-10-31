c174c65017be069f24242e353be01557c069b8d1
Stephan M. Unter <social@stephan-unter.de>
2022-10-31 10:29:10 +0100

    double pip install check for packages in python

--- python-scripts/python_test.py
+++ python-scripts/python_test.py
@@ -4,10 +4,16 @@ try:
     import numpy as np
     print("Python: Numpy available")
 except:
-    subprocess.check_call([sys.executable, '-m', 'pip', 'install', '--user', 'numpy'])
+    try:
+        subprocess.check_call([sys.executable, '-m', 'pip3', 'install', '--user', 'numpy'])
+    except:
+        subprocess.check_call([sys.executable, '-m', 'pip', 'install', '--user', 'numpy'])
 
 try:
     import PIL
     print("Python: PIL available")
 except:
-    subprocess.check_call([sys.executable, '-m', 'pip', 'install', '--user', 'pillow'])
+    try:
+        subprocess.check_call([sys.executable, '-m', 'pip3', 'install', '--user', 'pillow'])
+    except:
+        subprocess.check_call([sys.executable, '-m', 'pip', 'install', '--user', 'pillow'])
