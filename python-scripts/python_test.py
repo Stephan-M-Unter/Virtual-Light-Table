15829a80097736213065e59130f73d0220e7b6cd
Stephan M. Unter <Stephan-M-Unter@users.noreply.github.com>
2022-11-16 10:18:58 +0100

    adding exceptions to python script

--- python-scripts/python_test.py
+++ python-scripts/python_test.py
@@ -17,8 +17,12 @@ except:
         subprocess.check_call([sys.executable, '-m', 'pip3', 'install', '--user', 'numpy'])
         print('Package newly installed: numpy (pip3)')
     except:
-        subprocess.check_call([sys.executable, '-m', 'pip', 'install', '--user', 'numpy'])
-        print('Package newly installed: numpy (pip)')
+        try:
+            subprocess.check_call([sys.executable, '-m', 'pip', 'install', '--user', 'numpy'])
+            print('Package newly installed: numpy (pip)')
+        except Exception as e:
+            print('Failure - could not install Numpy!')
+            print(e)
 
 try:
     import PIL
@@ -28,45 +32,41 @@ except:
         subprocess.check_call([sys.executable, '-m', 'pip3', 'install', '--user', 'pillow'])
         print('Package newly installed: pillow (pip3)')
     except:
-        subprocess.check_call([sys.executable, '-m', 'pip', 'install', '--user', 'pillow'])
-        print('Package newly installed: pillow (pip)')
+        try:
+            subprocess.check_call([sys.executable, '-m', 'pip', 'install', '--user', 'pillow'])
+            print('Package newly installed: pillow (pip)')
+        except Exception as e:
+            print('Failure - could not install Pillow')
+            print(e)
 
 if platform == "darwin":
-    STAT_0o775 = ( stat.S_IRUSR | stat.S_IWUSR | stat.S_IXUSR
-                | stat.S_IRGRP | stat.S_IWGRP | stat.S_IXGRP
-                | stat.S_IROTH |                stat.S_IXOTH )
-
-    openssl_dir, openssl_cafile = os.path.split(
-        ssl.get_default_verify_paths().openssl_cafile)
-
-    print(" -- pip install --upgrade certifi")
-    subprocess.check_call([sys.executable,
-        "-E", "-s", "-m", "pip", "install", "--upgrade", "certifi"])
-
-    import certifi
-
-    # change working directory to the default SSL directory
-    os.chdir(openssl_dir)
-    relpath_to_certifi_cafile = os.path.relpath(certifi.where())
-    print(" -- removing any existing file or link")
     try:
-        os.remove(openssl_cafile)
-    except FileNotFoundError:
-        pass
-    print(" -- creating symlink to certifi certificate bundle")
-    os.symlink(relpath_to_certifi_cafile, openssl_cafile)
-    print(" -- setting permissions")
-    os.chmod(openssl_cafile, STAT_0o775)
-    print(" -- update complete")
+        STAT_0o775 = ( stat.S_IRUSR | stat.S_IWUSR | stat.S_IXUSR
+                    | stat.S_IRGRP | stat.S_IWGRP | stat.S_IXGRP
+                    | stat.S_IROTH |                stat.S_IXOTH )
 
-    """
-    try:
+        openssl_dir, openssl_cafile = os.path.split(
+            ssl.get_default_verify_paths().openssl_cafile)
+
+        print(" -- pip install --upgrade certifi")
+        subprocess.check_call([sys.executable,
+            "-E", "-s", "-m", "pip", "install", "--upgrade", "certifi"])
+
         import certifi
-    except:
+
+        # change working directory to the default SSL directory
+        os.chdir(openssl_dir)
+        relpath_to_certifi_cafile = os.path.relpath(certifi.where())
+        print(" -- removing any existing file or link")
         try:
-            subprocess.check_call([sys.executable, '-m', 'pip3', 'install', '--upgrade', '--user', 'certifi'])
-            print('Package newly installed: certifi (pip3)')
-        except:
-            subprocess.check_call([sys.executable, '-m', 'pip', 'install', '--upgrade', '--user', 'certifi'])
-            print('Package newly installed: certifi (pip)')
-    """
+            os.remove(openssl_cafile)
+        except FileNotFoundError:
+            pass
+        print(" -- creating symlink to certifi certificate bundle")
+        os.symlink(relpath_to_certifi_cafile, openssl_cafile)
+        print(" -- setting permissions")
+        os.chmod(openssl_cafile, STAT_0o775)
+        print(" -- update complete")
+    except Exception as e:
+        print("Failure - problem while installing certifi")
+        print(e)
