b281dbbd84db7753195b0c9c2d8e9ec49abf93ed
Stephan M. Unter <social@stephan-unter.de>
2023-08-15 12:34:54 +0200

    updates webpage

--- docs/script.js
+++ docs/script.js
@@ -10,3 +10,17 @@ $('.faq-item').click((event) => {
 $('.faq-item .question').click((event) => {
     $(event.target).closest('.faq-item').click();
 });
+
+$('.bug-shorts').click((event) => {
+    const bugDescription = $(event.target).next('.bug-description');
+    bugDescription.toggleClass('expanded');
+});
+
+$('.bug-cell').click((event) => {
+    const bugDescription = $(event.target).parent().parent().find('.bug-description');
+    bugDescription.toggleClass('expanded');
+});
+
+$('.home').click(() => {
+    window.location.href = 'index.html';
+});
